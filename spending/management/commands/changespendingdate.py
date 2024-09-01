5cef29dc37ef35028dba7ade0ad055b1759f8899
DaniilSh23 <danyasevas11@gmail.com>
2024-09-01 16:34:29 +0300

    команда переноса трат на вчера

---
+++ spending/management/commands/changespendingdate.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+from loguru import logger
+from datetime import datetime, timedelta
+from spending.models import Spending
+
+
+class Command(BaseCommand):
+    """
+    Команда для изменения даты и времени записи, относительно N кол-во дней от текущей даты.
+    """
+    def handle(self, *args, **options):
+        logger.info(f'Старт команды для изменения даты и времени записи.')
+
+        today = datetime.today()
+        yesterday = today - timedelta(days=1)
+        spendings_today = Spending.objects.filter(created_at__date=today)
+        spendings_today.update(created_at=yesterday)
